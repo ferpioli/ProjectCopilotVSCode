b137bf544e92ba53246fd9e9fc7484b82982a2f0
ferpioli <ferpiolli@gmail.com>
2023-06-17 15:26:48 -0300

    Usando GitHub Copilot Labs

--- Sketch.js
+++ Sketch.js
@@ -1,36 +1,52 @@
-//posicao do circulo
-var x = 200;
-var y = 200;
+class Bola{
+    constructor(x,y,vx,vy){
+        this.x = Math.random() * width;
+        this.y = Math.random() * height;
+        this.r = 25;
+        this.reset();
+        this.vx = Math.random() * 10 - 5;
+        this.vy = Math.random() * 10 - 5;
+    }
+    reset(){
+        this.x = width/2;
+        this.y = height/2;  
+        this.vx = Math.random() * 10 - 5;
+        this.vy = Math.random() * 10 - 5;
+    }
+    update(){
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
+        if(this.x <this.r || this.x > width - this.r){
+            this.reset();
+        }
 
-//velocidade do circulo
-var vx = Math.floor(Math.random() * 10) + 1;
-var vy = Math.floor(Math.random() * 10) + 1;
-
-
+        // se tocar na parede, inverte a velocidade
+        if (this.x < 25 || this.x > width -25) {
+            this.vx = this.vx * -1;
+        }
+        //idem para a parede de cima e de baixo
+        if (this.y < 25 || this.y > height -25) {
+            this.vy = this.vy * -1;
+        }
+    }
+    desenha(){
+        fill(255);
+        ellipse(this.x, this.y, 50, 50);  
+    }
+}
+var bola;
 // funcao setup do p5Js
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 400);
+    bola = new Bola(200,200,Math.floor(Math.random() * 10) + 1,Math.floor(Math.random() * 10) + 1);
    
     }
 
 
 //funcao draw do p5Js
 function draw() {
-//desenha um circulo
+//desenha uma bola
     background(0);
-    fill(255);
-    ellipse(x, y, 50, 50);  
-    x = x + vx;
-    y = y + vy;
-
-  // se tpocar na parede, inverte a velocidade
-    if (x < 25 || x > 375) {
-        vx = vx * -1;
-    }
-    //idem para a parede de cima e de baixo
-    if (y < 25 || y > 375) {
-        vy = vy * -1;
-    }
-
+    bola.desenha();
+    bola.update();
 }
-
---
+++ readme.md
@@ -0,0 +1,3 @@
+Extrair uma classe chamada Bola para representar a posição e movimento e update e desenho da mesma
+
+quando toca na borda em x reseta velocidade tambem atraves de função reset
